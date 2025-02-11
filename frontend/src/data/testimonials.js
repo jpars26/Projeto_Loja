8938503ad82f4c2b245eb06bee04084b20967f50
jpars26 <jpars131@gmail.com>
2025-02-11 16:56:52 -0300

    new-test

--- frontend/src/data/testimonials.js
+++ frontend/src/data/testimonials.js
@@ -2,27 +2,27 @@ const testimonials = [
     {
       name: "Amanda Brandao",
       text: "O vestido dos meus sonhos! A Iara Noivas fez com que meu casamento fosse ainda mais especial.",
-      image: require("../assets/images/amandaBrandao.png") 
+      image: require("../assets/images/amanda.png") 
     },
     {
       name: "Elisa Melo",
       text: "Desde o primeiro atendimento, fui tratada com muito carinho. Meu vestido ficou perfeito!",
-      image: require("../assets/images/elisaMelo.jpg") 
+      image: require("../assets/images/elisa.jpg") 
     },
     {
       name: "Bruna Alves",
       text: "A experiência na Iara Noivas foi incrível! Me senti uma princesa no dia do meu casamento.",
-      image: require("../assets/images/brunaAlves.jpg") 
+      image: require("../assets/images/bruna.jpg") 
      },
     {
       name: "Letícia Ferreira",
       text: "O atendimento foi excepcional e o vestido superou minhas expectativas!",
-      image: require("../assets/images/leticiaFerreira.jpg") 
+      image: require("../assets/images/leticia.jpg") 
     },
     {
       name: "Jacqueline Machado",
       text: "Sem palavras para descrever minha felicidade com o vestido perfeito para o meu grande dia!",
-      image: require("../assets/images/jaquelineMachado.jpg") 
+      image: require("../assets/images/jaqueline.jpg") 
     },
     {
       name: "Helizabelle",
